6a79269a9048f98d746b90690e06de4c623bd3ba
Aurnob <aurnob@Aurnobs-MacBook-Air.local>
2024-05-30 15:28:22 +0600

    p

---
+++ conf.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+def process(df,options):
+    
+    def mapping(feature):
+        return feature.map({'Yes':1,'No':0})
+    
+    cat_cols=['SeniorCitizen', 'Dependents', 'PhoneService', 'PaperlessBilling']
+    
+    df['cat_cols']=df[cat_cols].apply(mapping)
+    if ( options=='Online'):
+        columns=['SeniorCitizen', 'Dependents', 'tenure', 'PhoneService', 'PaperlessBilling', 'MonthlyCharges', 'TotalCharges',
+                 'MultipleLines_No_phone_service', 'MultipleLines_Yes', 'InternetService_Fiber_optic', 'InternetService_No', 
+                 'OnlineSecurity_No_internet_service', 'OnlineSecurity_Yes', 'OnlineBackup_No_internet_service', 
+                 'TechSupport_No_internet_service', 'TechSupport_Yes', 'StreamingTV_No_internet_service', 'StreamingTV_Yes', 
+                 'StreamingMovies_No_internet_service', 'StreamingMovies_Yes', 'Contract_One_year', 'Contract_Two_year', 'PaymentMethod_Electronic_check']
+        df=pd.get_dummies(df).reindex(columns=columns,fill_value=0)
+    elif (options=='Batch'):
+        columns=['SeniorCitizen', 'Dependents', 'tenure', 'PhoneService', 'PaperlessBilling', 'MonthlyCharges', 'TotalCharges',
+                 'MultipleLines_No_phone_service', 'MultipleLines_Yes', 'InternetService_Fiber_optic', 'InternetService_No',
+                 'OnlineSecurity_No_internet_service', 'OnlineSecurity_Yes', 'OnlineBackup_No_internet_service',
+                 'TechSupport_No_internet_service', 'TechSupport_Yes', 'StreamingTV_No_internet_service', 'StreamingTV_Yes',
+                 'StreamingMovies_No_internet_service', 'StreamingMovies_Yes', 'Contract_One_year', 'Contract_Two_year', 'PaymentMethod_Electronic_check']
+        df=pd.get_dummies(df).reindex(columns=columns, fill_value=0)
+        
+    
+    
+    

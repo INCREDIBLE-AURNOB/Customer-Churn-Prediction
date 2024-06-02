7541135688294973b1745574a0aa638d225b4d84
Aurnob <aurnob@Aurnobs-MacBook-Air.local>
2024-06-03 01:13:14 +0600

    last

---
+++ app.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+import joblib
+from conf import *
+
+model=joblib.load(r"model.sav")
+def main():
+    st.title("Customer Churn Prediction App")
+    
+    st.markdown("This app predicts the **Customer Churn**")
+    st.markdown("<h3></h3>", unsafe_allow_html=True)
+    
+    image=Image.open("image.jpeg")
+    add_selectbox=st.sidebar.selectbox(
+        "How would you like to predict?",
+        ("Online","Batch")
+    )
+    st.sidebar.info("This app is created to predict customer churn")
+    st.sidebar.image(image)
+    
+    if add_selectbox=="Online":
+        st.info("Please enter the informations")
+        st.subheader("Demographic Data")
+        seniorcitizen=st.selectbox("Senior Citizen",['Yes','No'])
+        dependents=st.selectbox("Dependents", ['Yes', 'No'])
+        
+        st.subheader("Payment")
+        tenure=st.slider('Number of months the customer has stayed with the company', min_value=0, max_value=72, value=0)
+        contact=st.selectbox('Contact', ['Month-to-month', 'One year', 'Two year'])
+        paperless=st.selectbox('Paperless Billing', ['Yes', 'No'])
+        paymentmethod=st.selectbox('Payment Method', ['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)']) 
+        monthlycharges=st.number_input('The amount charged to the customer monthly', min_value=0, max_value=150, value=0)
+        totalcharges=st.number_input('The total amount charged to the customer', min_value=0, max_value=10000, value=0)
+        
+        st.subheader("Services signed up for")
+        mutliplelines = st.selectbox("Does the customer have multiple lines",('Yes','No','No phone service'))
+        phoneservice = st.selectbox('Phone Service:', ('Yes', 'No'))
+        internetservice = st.selectbox("Does the customer have internet service", ('DSL', 'Fiber optic', 'No'))
+        onlinesecurity = st.selectbox("Does the customer have online security",('Yes','No','No internet service'))
+        onlinebackup = st.selectbox("Does the customer have online backup",('Yes','No','No internet service'))
+        techsupport = st.selectbox("Does the customer have technology support", ('Yes','No','No internet service'))
+        streamingtv = st.selectbox("Does the customer stream TV", ('Yes','No','No internet service'))
+        streamingmovies = st.selectbox("Does the customer stream movies", ('Yes','No','No internet service'))
+        
+        
+        data={
+            'SeniorCitizen': seniorcitizen,
+            'Dependents': dependents,
+            'tenure': tenure,
+            'PhoneService': phoneservice,
+            'PaperlessBilling': paperless,
+            'MonthlyCharges': monthlycharges,
+            'TotalCharges': totalcharges,
+            'MultipleLines': mutliplelines,
+            'InternetService_Fiber_optic': internetservice,
+            'OnlineSecurity_No_internet_service': onlinesecurity,
+            'OnlineBackup_No_internet_service': onlinebackup,
+            'TechSupport_No_internet_service': techsupport,
+            'StreamingTV_No_internet_service': streamingtv,
+            'StreamingMovies_No_internet_service': streamingmovies,
+            'Contract_One_year': contact,
+            'Contract_Two_year': contact,
+            'PaymentMethod_Electronic_check': paymentmethod
+        }
+        
+        features_df=pd.DataFrame.from_dict([data])
+        st.markdown("<h3></h3>", unsafe_allow_html=True)
+        st.write('Overview of customer data')
+        st.markdown("<h3></h3>", unsafe_allow_html=True)
+        st.dataframe(features_df)
+        
+        #Preprocess  the data
+        preprocess_df=process(features_df,'Online')
+        prediction=model.predict(preprocess_df)
+        
+    else:
+        st.subheader("Upload your file ") 
+        file=st.file_uploader("Upload CSV", type=['csv'])
+        if file is not None:
+            data=pd.read_csv(file)
+            st.markdown("<h3></h3>", unsafe_allow_html=True)
+            st.write('Overview of customer data')
+            st.markdown("<h3></h3>", unsafe_allow_html=True)
+            st.dataframe(data)
+
+            #Preprocess  the data
+            preprocess_df=process(data, 'Batch')
+            if st.button('Predict'):
+                prediction=model.predict(preprocess_df)
+                prediction_df=pd.DataFrame(prediction, columns=['Prediction'])
+                prediction_df = prediction_df.replace({1:'Yes, the customer will terminate the service.', 
+                                                    0:'No, the customer is happy with the Services.'})
+
+                st.markdown("<h3></h3>", unsafe_allow_html=True)
+                st.subheader('Prediction')
+                st.write(prediction_df)
+                
+    
+                
+if __name__=='__main__':
+    main()
